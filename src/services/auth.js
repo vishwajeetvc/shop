12487114e746f6ccac9b59e9d2fbd3be72612a89
Kishan Kumar <kishan2764@gmail.com>
2024-07-31 15:45:55 +0530

    add auth services

---
+++ src/services/auth.js
@@ -0,0 +1,20 @@
+export class AuthService{
+
+    async login({email,password}) {
+        return
+    }
+    async getCurrentUser() {
+        return
+    }
+    async logout() {
+        return
+    }
+    async changePassword({email, password}) {
+        return
+    }
+
+}
+
+const authService = new AuthService();
+
+export default authService;
